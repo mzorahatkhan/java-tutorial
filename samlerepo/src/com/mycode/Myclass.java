c900ae5b35e072f0cbc73e19b183d7a8a1025bd9
amzadhossain <amzadhossain@amzads-mbp.lan>
2023-03-20 12:05:56 -0400

    hi i changed something in feature and development

--- samlerepo/src/com/mycode/Myclass.java
+++ samlerepo/src/com/mycode/Myclass.java
@@ -4,6 +4,7 @@ public class Myclass {
 
 	public static void main(String[] args) {
 		System.out.println("this is my code");
+		System.out.println("this is the code i changed in my feature");
 
 	}
 
