40db2b68a98436b51d68c7ddbe1df2c6b1e76c11
mzorahatkhan <mzorahatqa@gmail.com>
2023-03-20 21:35:02 -0400

    this to checke email

--- samlerepo/src/com/mycode/Myclass.java
+++ samlerepo/src/com/mycode/Myclass.java
@@ -5,6 +5,7 @@ public class Myclass {
 	public static void main(String[] args) {
 		System.out.println("this is my code");
 		System.out.println("this is the code i changed in my feature");
+		System.out.println("to check my email");
 
 	}
 
